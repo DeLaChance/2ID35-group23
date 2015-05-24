83211bbf9c219bfb26190594b2724e3c4b9815cd
Francois van der Ven <francoisvdven@gmail.com>
2015-05-24 12:33:10 +0200

    C1P

--- TwigQuerySelectivity23/src/c1p/C1P.java
+++ TwigQuerySelectivity23/src/c1p/C1P.java
@@ -6,19 +6,51 @@
 package c1p;
 
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 /**
  *
  * @author francois
  */
 public class C1P {
+	private static class OverlapComparator implements Comparator<Row> {
+		@Override
+		public int compare(Row a, Row b) {
+			return a.getOverlap() < b.getOverlap() ? -1 : a.getOverlap() == b.getOverlap() ? 0 : 1;
+		}
+	}
+	
 	public c1p.Matrix heuristic(labeler.Matrix inputMatrix) {
-		Row firstRow = (Row)inputMatrix.getRow(0);
+		List<Row> matrix = new ArrayList<Row>();
 		
-		for(int i = 1; i < inputMatrix.getMatrix().size(); i++)
+		Row firstRow = (Row)inputMatrix.getRow(0);
+		matrix.add(firstRow);
+		
+		//Calculate overlap
 		{
-			Row row = (Row)inputMatrix.getRow(i);
-			
+			//For each row except the first
+			for(int i = 1; i < inputMatrix.getMatrix().size(); i++)
+			{
+				Row row = (Row)inputMatrix.getRow(i);
+
+				//For each column
+				for(int j = 0; j < row.size(); i++)
+				{				
+					boolean v1 = (firstRow.get(j) != 0);
+					boolean v2 = (row.get(j) != 0);
+
+					if(v1 && v2)
+						row.setOverlap(row.getOverlap() + 1);
+				}
+
+				matrix.add(row);
+			}
+		}
+		
+		//Sort rows on overlap
+		{
+			matrix.sort(new OverlapComparator());
 		}
 		
 		return null;
