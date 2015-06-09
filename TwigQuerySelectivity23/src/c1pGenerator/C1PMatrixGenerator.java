4d3bba1556bb5951dcef660a8baab2e216b63d90
Francois van der Ven <francoisvdven@gmail.com>
2015-06-09 18:43:26 +0200

    Attempt at matrix generator

--- TwigQuerySelectivity23/src/c1pGenerator/C1PMatrixGenerator.java
+++ TwigQuerySelectivity23/src/c1pGenerator/C1PMatrixGenerator.java
@@ -8,6 +8,9 @@ package c1pGenerator;
 
 import c1p.C1PMatrix;
 import c1p.C1PRow;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 /**
  *
@@ -38,6 +41,111 @@ public class C1PMatrixGenerator
         return m;
     }
     
+	public static C1PMatrix createMatrix2()
+	{
+		//Create a random number of trees
+		C1PMatrix m = new C1PMatrix();
+		
+		int offset = 0;
+		for(int i = 0; i < 100; i++)
+		{
+			List<C1PRow> rows = createTree(200, randInt(0, 100), randInt(0, 10));
+			
+			int maxX = offset;
+			for(C1PRow row : rows)
+			{		
+				C1PRow newRow = new C1PRow(null, row.getX() + offset, row.getY() + offset);
+				m.add(newRow);
+				
+				if(newRow.getY() > maxX)
+					maxX = newRow.getY();
+			}
+			
+			offset = maxX;
+		}
+		
+		return m;
+	}
+	public static List<C1PRow> createTree(int columns, int leafs, int treeHeight)
+	{
+		List<C1PRow> r = new ArrayList<>();
+		
+		if(treeHeight < 1)
+			return r;
+		
+		List<C1PRow> newRows = new ArrayList<>();
+		newRows.add(createRow(columns - leafs, leafs)); //add leafs
+		for(int level = treeHeight - 2; level >= 0; level--)
+		{
+			//Build a tree around the leafs
+			C1PRow previousRow = newRows.get(newRows.size() - 1);
+			int columnsLeft = columns - (previousRow.getY() - previousRow.getX());
+			if(columnsLeft == 0)
+				break;
+
+			C1PRow newRow = null;
+			
+			int levelsLeft = level; //for clarity
+			if(columnsLeft == levelsLeft) {
+				//Add one 1 per row
+				if(previousRow.getY() == columns - 1)
+					newRow = previousRow.prependOne();
+				else if(previousRow.getX() == 0)
+					newRow = previousRow.appendOne();
+				else
+					newRow = randBool() ? previousRow.prependOne() : previousRow.appendOne();
+			}
+			else if(columnsLeft < levelsLeft) { 
+				//Add  zero or one 1 per row
+				boolean add = randBool();
+				
+				if(add) { //add a new row with a 1 prepended or appended
+					if(previousRow.getY() == columns - 1)
+						newRow = previousRow.prependOne();
+					else if(previousRow.getX() == 0)
+						newRow = previousRow.appendOne();
+					else
+						newRow = randBool() ? previousRow.prependOne() : previousRow.appendOne();
+				}
+				else //simply copy the previous row
+					newRow = new C1PRow(previousRow);
+			} 
+			else if(columnsLeft > levelsLeft) {
+				//Add more than one 1 per row
+				int maxOnesToAdd = columnsLeft - levelsLeft;
+				int onesToAdd = randInt(1, maxOnesToAdd);
+				
+				C1PRow c = new C1PRow(previousRow);
+				for(int i = 0; i < onesToAdd; i++)
+				{
+					if(c.getY() == columns - 1)
+						c = c.prependOne();
+					else if(c.getX() == 0)
+						c = c.appendOne();
+					else
+						c = randBool() ? c.prependOne() : c.appendOne();
+				}
+				newRow = c;
+			}
+			
+			newRows.add(newRow);
+		}
+		
+		while(!newRows.isEmpty())
+		{
+			r.add(newRows.remove(newRows.size() - 1));
+		}
+		
+		return r;
+	}
+	
+	private static C1PRow createRow(int maxX, int width)
+	{
+		int x = randInt(0, maxX);
+		int y = x + width;
+		return new C1PRow(null, x, y);
+	}
+	
     // random int in range [min, max) 
     // i.e. including min, excluding max
     private static int randInt(int min, int max)
@@ -54,4 +162,9 @@ public class C1PMatrixGenerator
         double r = Math.random();
         return min+(int)((max-min)*r*r*r);
     }
+	
+	private static boolean randBool()
+	{
+		return new Random().nextBoolean();
+	}
 }
--- TwigQuerySelectivity23/src/twigqueryselectivity23/Test.java
+++ TwigQuerySelectivity23/src/twigqueryselectivity23/Test.java
@@ -6,9 +6,12 @@
 
 package twigqueryselectivity23;
 
+import c1p.C1PMatrix;
+import c1p.C1PRow;
 import c1pGenerator.C1PMatrixGenerator;
 import hist.Histogram;
 import hist.debugView.MainView;
+import java.util.List;
 
 /**
  *
@@ -18,9 +21,16 @@ public class Test
 {
     public static final void main(String[] args) throws Throwable
     {
+		testMatrixGenerator();
         testHistogram();
     }
     
+	public static void testMatrixGenerator()
+	{
+		List<C1PRow> m = C1PMatrixGenerator.createTree(10, 3, 4);
+		System.out.println("Done");
+	}
+	
     public static void testHistogram()
     {
         Histogram hist = new Histogram(C1PMatrixGenerator.createMatrix());
